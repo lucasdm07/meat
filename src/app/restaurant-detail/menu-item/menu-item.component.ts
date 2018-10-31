d86071a3790b96a9e7e2d056c7a3758ba8fc02d5
lucasdm08 <44589860+lucasdm08@users.noreply.github.com>
2018-10-30 22:32:13 -0300

    CONCLUSAO DO CURSO DE ANIMATIONS

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -6,17 +6,19 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   selector: 'mt-menu-item',
   templateUrl: './menu-item.component.html',
   animations: [
-    trigger('restaurantAppeared', [
+    trigger('menuItemAppeared', [
       state('ready', style({opacity: 1})),
       transition('void => ready', [
-        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
-        animate('500ms 0s ease-in-out')
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in')
       ])
     ])
   ]
 
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready';
 
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter();
